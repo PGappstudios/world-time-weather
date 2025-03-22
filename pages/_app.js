2065112e67b071b3e34f2e845b7974122c108717
PGappstudios <pgmetastudios@gmail.com>
2025-03-22 18:25:17 +0000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,5 +3,6 @@ import '../styles/globals.css';
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
-
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9894683094010434"
+     crossorigin="anonymous"></script>
 export default MyApp; 
