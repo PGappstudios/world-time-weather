07d817f1d0d4b87979bd9509933cab7d403603d6
pg <pg>
2025-03-22 19:48:36 +0000

    Update AdSense implementation for Auto ads

--- components/Adsense.js
+++ components/Adsense.js
@@ -3,11 +3,14 @@ import { useEffect } from 'react';
 export default function Adsense({ clientId }) {
   useEffect(() => {
     try {
-      (window.adsbygoogle = window.adsbygoogle || []).push({});
+      (window.adsbygoogle = window.adsbygoogle || []).push({
+        google_ad_client: clientId,
+        enable_page_level_ads: true
+      });
     } catch (err) {
       console.error('AdSense error:', err);
     }
-  }, []);
+  }, [clientId]);
 
   return (
     <>
@@ -15,6 +18,7 @@ export default function Adsense({ clientId }) {
         async
         src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${clientId}`}
         crossOrigin="anonymous"
+        data-ad-client={clientId}
       />
     </>
   );
